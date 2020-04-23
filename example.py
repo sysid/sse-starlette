72b7d0392d2faff4d7541d62b37819e627ae85f5
sysid <thomas.we.weber@gmx.de>
2020-04-23 15:26:44 +0200

    chore: victors example

--- example.py
+++ example.py
@@ -3,6 +3,7 @@ import logging
 
 import uvicorn
 from starlette.applications import Starlette
+from starlette.requests import Request
 from starlette.routing import Route
 
 from sse_starlette.sse import EventSourceResponse
@@ -16,8 +17,17 @@ async def numbers(minimum, maximum):
         yield dict(data=i)
 
 
+async def endless_numbers():
+    i = 0
+    while True:
+        await asyncio.sleep(0.3)
+        i += 1
+        yield dict(data=i)
+
+
 async def sse(request):
-    generator = numbers(1, 5)
+    # generator = numbers(1, 5)
+    generator = endless_numbers()
     return EventSourceResponse(generator)
 
 
@@ -26,6 +36,20 @@ routes = [
 ]
 
 app = Starlette(debug=True, routes=routes)
+
+
+@app.route('/streaming-endpoint', methods=['GET'])
+async def stream_stats(req: Request):
+    async def event_publisher():
+        i = 0
+        while True:
+            # yield dict(id=..., event=..., data=...)
+            i += 1
+            yield dict(data=i)
+            await asyncio.sleep(0.2)
+
+    return EventSourceResponse(event_publisher())
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level='info')
