d148fc288870bea6785f999b6e62232506d65ec5
sysid <sysid@gmx.de>
2022-11-15 20:27:33 +0100

    added example and test for custom_generator

---
+++ examples/custom_generator.py
@@ -0,0 +1,43 @@
+import asyncio
+
+from fastapi import FastAPI, Depends
+from sse_starlette import EventSourceResponse, ServerSentEvent
+from starlette import status
+
+
+class Stream:
+    def __init__(self) -> None:
+        self._queue = asyncio.Queue[ServerSentEvent]()
+
+    def __aiter__(self) -> "Stream":
+        return self
+
+    async def __anext__(self) -> ServerSentEvent:
+        return await self._queue.get()
+
+    async def asend(self, value: ServerSentEvent) -> None:
+        await self._queue.put(value)
+
+
+app = FastAPI()
+
+_stream = Stream()
+app.dependency_overrides[Stream] = lambda: _stream
+
+
+@app.get("/sse")
+async def sse(stream: Stream = Depends()) -> EventSourceResponse:
+    return EventSourceResponse(stream)
+
+
+@app.post("/message", status_code=status.HTTP_201_CREATED)
+async def send_message(message: str, stream: Stream = Depends()) -> None:
+    await stream.asend(
+        ServerSentEvent(data=message)
+    )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
