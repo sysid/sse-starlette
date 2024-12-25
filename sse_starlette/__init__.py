32c7d530b5a821a38650d786f11ecdf0abc7121d
sysid <sysid@gmx.de>
2024-12-25 10:07:29 +0100

    fix: https://github.com/sysid/sse-starlette/issues/120

--- sse_starlette/__init__.py
+++ sse_starlette/__init__.py
@@ -1,4 +1,5 @@
+from sse_starlette.event import ServerSentEvent
 from sse_starlette.sse import EventSourceResponse
 
-__all__ = ["EventSourceResponse"]
+__all__ = ["EventSourceResponse", "ServerSentEvent"]
 __version__ = "2.2.0"
