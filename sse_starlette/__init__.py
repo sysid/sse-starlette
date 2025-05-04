81352aeaa71b6b4b5f7643e930d772a7ba42ad87
sysid <sysid@gmx.de>
2025-05-04 21:26:44 +0200

    Bump version to 2.3.4

--- sse_starlette/__init__.py
+++ sse_starlette/__init__.py
@@ -2,4 +2,4 @@ from sse_starlette.event import ServerSentEvent
 from sse_starlette.sse import EventSourceResponse
 
 __all__ = ["EventSourceResponse", "ServerSentEvent"]
-__version__ = "2.3.3"
+__version__ = "2.3.4"
